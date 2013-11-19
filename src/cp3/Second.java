7f468671cd4907320c46e24ac9a03773f4944364
zhufengxiang <appreciate.slow@gmail.com>
2013-11-19 20:51:11 +0800

    second

---
+++ src/cp3/Second.java
@@ -0,0 +1,15 @@
+package cp3;
+
+public class Second {
+/**
+ *   /除 跟 %取模区分
+ *   前缀自增、后缀自增区分
+ *   前缀自减、后缀自减区分
+ *   三元操作
+ * @param args
+ */
+	public static void main(String[] args) {
+
+	}
+
+}
