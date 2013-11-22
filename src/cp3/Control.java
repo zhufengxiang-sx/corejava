4a3aeb15b39ce3c2d0b04eb2dc07670e764ae34c
zhufengxiang <appreciate.slow@gmail.com>
2013-11-22 20:51:36 +0800

    块

--- src/cp3/Control.java
+++ src/cp3/Control.java
@@ -7,11 +7,20 @@ package cp3;
  * 	块的概念:由一对花括号括起来的若干条简单的Java语句。
  * 	作用：确定变量的作用域。
  * 
- * 一个块可以嵌套在另一个块中。但是不能在嵌套的两个块中声明同名的变量。
+ *
  *
  */
 public class Control {
-	public static void main(String[] args) {
-
+	
+	public static void main(String[] args) {  // 一个块可以嵌套在另一个块中。
+		int k = 11;
+		{
+			int n = 12;
+			System.out.println(k);
+			System.out.println(n);
+			//int k = 12;                    但是不能在嵌套的两个块中声明同名的变量。
+		}
+		System.out.println(k);
+		//System.out.println(n);
 	}
 }
